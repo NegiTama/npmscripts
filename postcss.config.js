6f3b99367463d94d45e65640fb5addd4bb374eb2
Shinichi Hieida <mcc.hieida@gmail.com>
2020-08-26 05:33:43 +0900

    gitは空のディレクトリはそのままリポジトリに上げる事はできない

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
       browsers: ['last 2 versions'],
       cascade: true,
       grid: "autoplace"
-    }),
+    })
     // 圧縮するなら
     // require('cssnano')({
     //   preset: 'default',
